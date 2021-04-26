6e80fd13996a3f0ff8e1fdebb7e82ddfde4d4056
Abhi7410 <82524461+Abhi7410@users.noreply.github.com>
2021-04-26 22:21:06 +0530

    Create treesl.h

---
+++ Individual Files/Abhishek/treesl.h
@@ -0,0 +1,25 @@
+#ifndef __treesearchlibrary__
+#define __treesearchlibrary__
+#include<stdbool.h>
+typedef struct node
+{
+    int state_number;
+    int value;
+    int parent;
+
+    // You (as a developer) can add any extra information here [Will not be touched by user]
+    int depth, seen_time, num_children;
+    struct node* children[10000];
+} Node;
+
+typedef struct node* PtrNode;
+void input_node(Node* a);
+void insertNode(PtrNode * ParentArr,PtrNode new, int n);
+PtrNode* Initialise_Tree( int n);
+
+
+
+
+
+
+#endif
