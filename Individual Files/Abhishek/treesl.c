825d5cf8994458287978c47e12622fdbf126dec8
Abhi7410 <82524461+Abhi7410@users.noreply.github.com>
2021-05-03 13:15:25 +0530

    Update treesl.c

--- Individual Files/Abhishek/treesl.c
+++ Individual Files/Abhishek/treesl.c
@@ -1,7 +1,7 @@
 #include "treesl.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 bool node_comparator(Node *a, Node *b)
 {
@@ -9,47 +9,81 @@ bool node_comparator(Node *a, Node *b)
     return (a->value < b->value); // A sample comparator
 }
 
-void input_node(Node* a)
+PtrNode assign_node(int d,int b,int c)
 {
-    scanf("%d %d %d",&(a->state_number),&(a->value),&(a->parent));
+    PtrNode patakha = (PtrNode)malloc(sizeof(Node));
+    patakha->state_number = d;
+    patakha->value = b;
+    patakha->parent = c;
+    patakha->seen_time = 0;
+    return patakha;
 }
 
 
+PtrNode Initialise_Tree(int n, PtrNode* ParentArr)
+{
+  PtrNode head = NULL;
+  PtrNode new_bomb;
+  for (int i = 0; i < n; i++)
+  {
+     int sn,val,parent;
+     scanf("%d %d %d",&sn,&val,&parent);
+     new_bomb = assign_node(sn,val,parent);
+     insertNode(ParentArr,new_bomb,n);
 
-void insertNode(PtrNode * ParentArr,PtrNode new, int n)
+     if (i==0)
+     {
+        head = new_bomb;
+     }
+  } 
+ return head;
+}
+
+void insertNode(PtrNode *ParentArr, PtrNode new, int n)
 {
     int Depth;
     if (new->parent == -1)
     {
-       ParentArr[0] = new;
-       return;
+        ParentArr[0] = new;
+        return;
     }
     int temp = 0;
-    while (ParentArr[temp]!=NULL && temp<n-1 && ParentArr[temp]->state_number!= new->parent)
+    while (ParentArr[temp] != NULL && temp < n - 1 && ParentArr[temp]->state_number != new->parent)
     {
-      temp++;
+        temp++;
     }
     if (ParentArr[temp]->state_number == new->parent)
     {
         int i = 0;
-        while (ParentArr[temp]->children[i]!=NULL)
+        while (ParentArr[temp]->children[i] != NULL)
         {
             i++;
         }
         ParentArr[temp]->children[i] = new;
         ParentArr[temp]->num_children++;
         Depth = ParentArr[temp]->depth;
-        new->depth = Depth +1;
+        new->depth = Depth + 1;
 
-        while (ParentArr[temp]!=NULL)
+        while (ParentArr[temp] != NULL)
         {
             temp++;
         }
         ParentArr[temp] = new;
     }
-    if (ParentArr[temp]==NULL)
-    {
-       printf("Parent not found\n");
-    }
-    
+ 
 }
+void print_Tree(PtrNode *ParentArr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d %d %d    \n", ParentArr[i]->state_number, ParentArr[i]->value, ParentArr[i]->parent);
+        int j = 0;
+        while (ParentArr[i]->children[j] != NULL)
+        {
+            printf("(%d %d %d )  ", ParentArr[i]->children[j]->state_number, ParentArr[i]->children[j]->value, ParentArr[i]->children[j]->parent);
+            j++;
+        }
+        printf("\n");
+    }
+}
+    
