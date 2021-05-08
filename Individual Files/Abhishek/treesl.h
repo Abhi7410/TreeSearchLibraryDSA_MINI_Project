9092931773644ca7e94f999a634152ec81332714
Abhi7410 <82524461+Abhi7410@users.noreply.github.com>
2021-05-08 09:20:44 +0530

    Update treesl.h

--- Individual Files/Abhishek/treesl.h
+++ Individual Files/Abhishek/treesl.h
@@ -11,16 +11,30 @@ typedef struct node
     int depth, seen_time, num_children;
     struct node* children[10000];
 } Node;
-
 typedef struct node* PtrNode;
+
+
+typedef struct pHeap
+{
+   int size;
+   int capacity;
+   PtrNode* Arr;  
+}pHeap;
+typedef struct pHeap* PtrHeap;
+
+int IsEmpty(PtrHeap P);
+void ProclateUp(PtrNode *Arr,int position,char* string);
+void ProclateDown(PtrNode* Arr,int position,int size,char* string);
+int DeleteTop(PtrHeap Pq,char* string);
+void HeapInsert(PtrHeap Pq,PtrNode new,char* string);
+PtrHeap Initialise_Heap(int capacity);
 PtrNode assign_node(int d,int b,int c);
 void insertNode(PtrNode * ParentArr,PtrNode new, int n);
-PtrNode Initialise_Tree(int n, PtrNode* ParentArr);
+void Initialise_Tree(int n, PtrNode* ParentArr);
 void print_Tree( PtrNode* ParentArr,int n);
+// bool DFScomparator(Node* a,Node* b);
+void heapify(PtrNode Arr[], int i,int size);
+// bool BFScomparator(Node* a,Node* b);
+bool comparator(Node* a,Node* b,char *string);
+void Print(PtrHeap P);
 
-
-
-
-
-
-#endif
