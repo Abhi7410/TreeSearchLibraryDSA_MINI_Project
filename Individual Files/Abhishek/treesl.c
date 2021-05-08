c277c536d8d0f6ee5137f078cae008b6801d4d12
Abhi7410 <82524461+Abhi7410@users.noreply.github.com>
2021-05-08 09:22:34 +0530

    Update treesl.c

--- Individual Files/Abhishek/treesl.c
+++ Individual Files/Abhishek/treesl.c
@@ -2,14 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-bool node_comparator(Node *a, Node *b)
+#include <string.h>
+bool comparator(Node *a, Node *b, char *string)
 {
-    // User puts in the node comparator here
-    return (a->value < b->value); // A sample comparator
+    if (strcmp(string,"BFS") == 0)
+    {
+        return (b->seen_time > a->seen_time);
+    }
+    if (strcmp(string,"DFS") == 0)
+    {
+        return (a->seen_time > b->seen_time);
+    }
+    if (strcmp(string,"GREEDY") == 0)
+    {
+        return (a->value > b->value);
+    }
+   
 }
 
-PtrNode assign_node(int d,int b,int c)
+void swap(PtrNode *a, PtrNode *b)
+{
+    PtrNode temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+PtrNode assign_node(int d, int b, int c)
 {
     PtrNode patakha = (PtrNode)malloc(sizeof(Node));
     patakha->state_number = d;
@@ -19,24 +37,23 @@ PtrNode assign_node(int d,int b,int c)
     return patakha;
 }
 
-
-PtrNode Initialise_Tree(int n, PtrNode* ParentArr)
+void Initialise_Tree(int n, PtrNode *ParentArr)
 {
-  PtrNode head = NULL;
-  PtrNode new_bomb;
-  for (int i = 0; i < n; i++)
-  {
-     int sn,val,parent;
-     scanf("%d %d %d",&sn,&val,&parent);
-     new_bomb = assign_node(sn,val,parent);
-     insertNode(ParentArr,new_bomb,n);
+    PtrNode head = NULL;
+    PtrNode new_bomb;
+    for (int i = 0; i < n; i++)
+    {
+        int sn, val, parent;
+        scanf("%d %d %d", &sn, &val, &parent);
+        new_bomb = assign_node(sn, val, parent);
+        insertNode(ParentArr, new_bomb, n);
 
-     if (i==0)
-     {
-        head = new_bomb;
-     }
-  } 
- return head;
+        if (i == 0)
+        {
+            head = new_bomb;
+        }
+    }
+    return;
 }
 
 void insertNode(PtrNode *ParentArr, PtrNode new, int n)
@@ -52,6 +69,7 @@ void insertNode(PtrNode *ParentArr, PtrNode new, int n)
     {
         temp++;
     }
+
     if (ParentArr[temp]->state_number == new->parent)
     {
         int i = 0;
@@ -70,13 +88,12 @@ void insertNode(PtrNode *ParentArr, PtrNode new, int n)
         }
         ParentArr[temp] = new;
     }
- 
 }
 void print_Tree(PtrNode *ParentArr, int n)
 {
     for (int i = 0; i < n; i++)
     {
-        printf("%d %d %d    \n", ParentArr[i]->state_number, ParentArr[i]->value, ParentArr[i]->parent);
+        printf("%d %d %d    (%d)\n", ParentArr[i]->state_number, ParentArr[i]->value, ParentArr[i]->parent, ParentArr[i]->num_children);
         int j = 0;
         while (ParentArr[i]->children[j] != NULL)
         {
@@ -85,5 +102,84 @@ void print_Tree(PtrNode *ParentArr, int n)
         }
         printf("\n");
     }
+}
+
+PtrHeap Initialise_Heap(int capacity)
+{
+    PtrHeap Pq = (PtrHeap)malloc(sizeof(pHeap));
+    Pq->size = 0;
+    Pq->capacity = capacity;
+    Pq->Arr = (PtrNode *)malloc(capacity * sizeof(PtrNode));
+    return Pq;
+}
+void HeapInsert(PtrHeap Pq, PtrNode new,char* string)
+{
+    Pq->Arr[Pq->size] = new;
+    ProclateUp(Pq->Arr, Pq->size,string);
+    Pq->size++;
+}
+void ProclateUp(PtrNode *Arr, int position,char* string)
+{
+    int parent = (position - 1) / 2;
+    if (position != 0)
+    {
+        if (comparator(Arr[position], Arr[parent],string))
+        {
+            PtrNode temp = Arr[position];
+            Arr[position] = Arr[parent];
+            Arr[parent] = temp;
+
+            ProclateUp(Arr, parent,string);
+        }
+    }
+}
+int DeleteTop(PtrHeap Pq,char* string)
+{
+    int value = Pq->Arr[0]->state_number;
+    Pq->Arr[0] = Pq->Arr[Pq->size - 1];
+    Pq->size--;
+    ProclateDown(Pq->Arr, 0, Pq->size,string);
+    return value;
+}
+void ProclateDown(PtrNode *Arr, int position, int size,char* string)
+{
+    int lchild = 2 * position + 1;
+    int rchild = lchild + 1;
+    int small = -1;
+    if (lchild < size)
+    {
+        small = lchild;
+    }
+    if (rchild < size && comparator(Arr[rchild], Arr[lchild],string))
+    {
+        small = rchild;
+    }
+    if (small != -1 && comparator(Arr[small], Arr[position],string))
+    {
+        PtrNode temp = Arr[position];
+        Arr[position] = Arr[small];
+        Arr[small] = temp;
+        ProclateDown(Arr, small,size,string);
+    }
+}
+int IsEmpty(PtrHeap P)
+{
+    if (P->size == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+void Print(PtrHeap P)
+{
+    for (int i = 0; i < P->size; i++)
+    {
+        printf("%d (%d) ", P->Arr[i]->state_number, P->Arr[i]->depth);
+    }
+    printf("\n");
 }
     
